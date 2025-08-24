cdcad41c94fd0af585464c4a61b2219ef1231513
taniachris8 <dove6464dove@gmail.com>
2025-08-24 10:57:18 +0300

    deleted if/else construction

--- src/Validator.js
+++ src/Validator.js
@@ -1,12 +1,7 @@
 export default class Validator {
-   
+
     validateUsername(userName) { 
         const regex = /^[a-z][\w-]*[a-z]$/i;
-
-        if (regex.test(userName) && !userName.match(/\d{4,}/)) {
-            return true;
-        } else { 
-            return false;
-        }
+        return regex.test(userName) && !userName.match(/\d{4,}/);
     }
 }
